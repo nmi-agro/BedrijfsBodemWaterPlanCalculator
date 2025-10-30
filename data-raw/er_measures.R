0e0a01d5368602e57409a4c6aee35b3685fd4f2b
Brent <72964508+BrentWHR@users.noreply.github.com>
2025-10-30 10:54:51 +0100

    rename csv

--- data-raw/er_measures.R
+++ data-raw/er_measures.R
@@ -2,7 +2,7 @@
 # -- prepare table for which ER measures can be used on which crops ----
 
 # load in csv  
-er_measures <- fread('dev/eco_brp.csv', encoding = 'UTF-8')
+er_measures <- fread('data-raw/er_measures.csv', encoding = 'UTF-8')
 
 # remove brp codes that do not occur in pandex
 er_measures <- er_measures[B_LU_BRP < 7000 & B_LU_BRP != 305,]
