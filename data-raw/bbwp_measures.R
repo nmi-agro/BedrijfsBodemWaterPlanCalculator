ce27f0f92f957088c7e0f5f81d51cba0de22885d
Brent <72964508+BrentWHR@users.noreply.github.com>
2025-11-07 15:41:45 +0100

    add fwrite call

--- data-raw/bbwp_measures.R
+++ data-raw/bbwp_measures.R
@@ -4,3 +4,4 @@ library(usethis)
 # Convert csv to RDA
 bbwp_measures <- fread('data-raw/bbwp_measures.csv')
 use_data(bbwp_measures, overwrite = TRUE)
+fwrite(bbwp_measures, 'data-raw/bbwp_measures.csv')
